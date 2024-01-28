3a05de16083abcb83814bf3b8d45787bd00c6dc6
Perz <percy.ali@highstreet.market>
2024-01-28 11:35:49 -0700

    PUSHHHHHHHHHHHH

--- Assets/Scripts/Other/Interactables/EndGameAction.cs
+++ Assets/Scripts/Other/Interactables/EndGameAction.cs
@@ -1,12 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class EndGameAction : MonoBehaviour, IButtonAction
 {
+    [SerializeField] private string sceneName;
+    [SerializeField] private bool quitGame = false;
+
     public void ExecuteAction()
     {
-        Debug.Log("Game Ended!");
+        if (quitGame)
+        {
+            Debug.Log("Quit Game!");
+            Application.Quit();
+
+            // If we are running in the Unity Editor
+            #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+            #endif
+        }
+        else
+        {
+            Debug.Log("Loading scene: " + sceneName);
+            SceneManager.LoadScene(sceneName);
+        }
     }
 }
-
--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -8,10 +8,25 @@ using TMPro;
 public class MainMenu : MonoBehaviour
 {
     public Fader fader;
+    [SerializeField] private AudioSource menuMusic; // Reference to the AudioSource for the menu music
+    [SerializeField] private float fadeOutDuration = 1f; // Duration for the music fade out
+
+    void Start()
+    {
+        if (menuMusic != null)
+        {
+            menuMusic.Play(); // Start playing the main menu music
+        }
+    }
 
     public void PlayGame()
     {
-        fader.FadeOut(() => SceneManager.LoadScene("Gym"));
+        if (menuMusic != null)
+        {
+            StartCoroutine(FadeOutMusic(fadeOutDuration)); // Start fading out the music
+        }
+
+        fader.FadeOut(() => SceneManager.LoadScene("02Level1"));
     }
 
     public void QuitGame()
@@ -19,5 +34,17 @@ public class MainMenu : MonoBehaviour
         Debug.Log("Quit!");
         Application.Quit();
     }
-}
 
+    private IEnumerator FadeOutMusic(float duration)
+    {
+        float startVolume = menuMusic.volume;
+
+        while (menuMusic.volume > 0)
+        {
+            menuMusic.volume -= startVolume * Time.deltaTime / duration;
+            yield return null;
+        }
+
+        menuMusic.Stop();
+    }
+}
